9e35d02ff3e3cf40b6536da67066a950108888e8
rosbel <rosbelcr@gmail.com>
2023-02-25 18:28:39 -0500

    NavBar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,41 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import logo from "../assets/Holguin-equipo.png";
+import { Badge, Button, Typography } from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
 
-export default function NavBar() {
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: "7rem",
+  },
+  appBar: {
+    backgroundColor: "whitesmoke",
+    boxShadow: "none",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  button: {
+    marginLeft: theme.spacing(2),
+  },
+  image: {
+    marginRight: "10px",
+    height: "3rem"
+  },
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
         <Toolbar>
           <IconButton
             size="large"
@@ -19,14 +44,23 @@ export default function NavBar() {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <img src={logo} className={classes.image} alt="logo-title"/>
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <div className={classes.grow}/>
+          <Typography variant="h6" color="textPrimary" component="p">Hello Gest</Typography>
+          
+          <div className={classes.button}>
+            <Button variant="outlined">
+            <strong>Sign In</strong>
+            </Button>
+            <IconButton aria-label="show cart items" color="inherit">
+              <Badge badgeContent={2} color="secondary">
+                <ShoppingCart fontSize="large" color="primary"/>
+              </Badge>  
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
-    </Box>
+    </div>
   );
-}
+};
