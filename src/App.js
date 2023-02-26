6e03dd80e722fe40cc56c9b26e06a6686e11e7bb
rosbel <rosbelcr@gmail.com>
2023-02-26 00:51:56 -0500

    grid done!

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Product from './components/Product';
+import Products from './components/Products';
 
 function App(){
   
   return (
     <>
     <NavBar/>
-    <Product/>
+    <Products/>
+    {/* <Product/> */}
     </>
     ) 
 }
